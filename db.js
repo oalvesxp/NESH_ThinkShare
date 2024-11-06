cfa057aa95dec358ef85d88011e60af4649b6578
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-06 11:22:33 -0300

    Marcando como visualizada e excluindo Notes expiradas

--- db.js
+++ db.js
@@ -17,11 +17,11 @@ const saveNote = (id, content) =>
   new Promise((res, rej) => {
     db.run(
       `
-      INSERT INTO notes
-        (id, content)
-      VALUES
-        (?, ?)
-    `,
+        INSERT INTO notes
+          (id, content)
+        VALUES
+          (?, ?)
+      `,
       [id, content],
       (err) => (err ? rej(err) : res())
     )
@@ -31,14 +31,43 @@ const getNote = (id) =>
   new Promise((res, rej) => {
     db.run(
       `
-    SELECT * FROM notes WHERE id = ?
-    `,
+        SELECT * FROM notes WHERE id = ?
+      `,
       [id],
       (err, row) => (err ? rej(err) : res(row))
+    )
+  })
+
+const markAsOpened = (id) =>
+  new Promise((res, rej) => {
+    db.run(
+      `
+        UPDATE notes
+        SET opened_at = datetime('now', 'localtime')
+        WHERE id = ?
+      `,
+      [id],
+      (err) => (err ? rej(err) : res())
+    )
+  })
+
+const deletedExpires = () =>
+  new Promise((res, rej) => {
+    db.run(
+      `
+        DELETE FROM notes
+        WHERE 
+          opened_at < datetime('now', 'localtime', '-5 minutes')
+          OR opened_at IS NULL 
+            AND created_at < datetime('now', 'localtime', '-7 days')
+      `,
+      (err) => (err ? rej(err) : res())
     )
   })
 
 module.exports = {
   saveNote,
   getNote,
+  markAsOpened,
+  deletedExpires,
 }
